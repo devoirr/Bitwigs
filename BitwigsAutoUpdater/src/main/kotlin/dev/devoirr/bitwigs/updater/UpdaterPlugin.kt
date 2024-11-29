870763315d276ba90f88708b5e9a5e0935fad5e1
romanrudoy <rudoy.roman@gmail.com>
2024-11-29 12:32:28 +0200

    Test update.

--- BitwigsAutoUpdater/src/main/kotlin/dev/devoirr/bitwigs/updater/UpdaterPlugin.kt
+++ BitwigsAutoUpdater/src/main/kotlin/dev/devoirr/bitwigs/updater/UpdaterPlugin.kt
@@ -3,9 +3,13 @@ package dev.devoirr.bitwigs.updater
 import org.apache.commons.io.IOUtils
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
 import java.net.URI
+import java.util.*
+import kotlin.math.log
 import kotlin.math.min
 
 class UpdaterPlugin: JavaPlugin() {
@@ -42,7 +46,13 @@ class UpdaterPlugin: JavaPlugin() {
         if (!outdated)
             return
 
+        logger.info("Downloading update...")
+        downloadUpdate()
 
+        logger.info("Done.")
+        logger.info("Restarting the server...")
+
+        Bukkit.getServer().shutdown()
 
     }
 
@@ -65,6 +75,27 @@ class UpdaterPlugin: JavaPlugin() {
 
     private fun downloadUpdate(): Boolean {
         val localFile = File("plugins/Bitwigs.jar")
+        if (localFile.exists()) {
+            localFile.delete()
+        }
+
+        localFile.createNewFile()
+        val url = URI("https://github.com/devoirr/Bitwigs/blob/main/BitwigsCore/build/libs/Bitwigs.jar").toURL()
+        val out = BufferedOutputStream(FileOutputStream(localFile))
+        val conn = url.openConnection()
+
+        val inputStream = conn.getInputStream()
+        val buffer = ByteArray(1024)
+
+        var numRead = inputStream.read(buffer)
+        while (numRead != -1) {
+            out.write(buffer, 0, numRead)
+            numRead = inputStream.read(buffer)
+        }
+
+        inputStream?.close()
+        out.close()
+
         return true
     }
 
